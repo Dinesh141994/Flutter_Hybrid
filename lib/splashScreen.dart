737b6cede41ead2b3082a5558b88e25ef078d5e4
Dinesh_Taral <dinesh.taral@zenways.io>
2020-01-15 15:04:25 +0530

    update

--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
 import 'package:example_flutter/rippleBackground.dart';
+import 'package:flare_dart/math/mat2d.dart';
+import 'package:flare_flutter/flare.dart';
 import 'package:flare_flutter/flare_actor.dart';
+import 'package:flare_flutter/flare_controller.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -13,7 +16,7 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
+    with SingleTickerProviderStateMixin , FlareController{
   AnimationController controller;
   Animation<double> scaleBounceAnim;
 
@@ -23,12 +26,30 @@ class _MyHomePageState extends State<SplashScreen>
   RippleBackground rippleBackground4;
   RippleBackground rippleBackground5;
 
+  double _rockAmount = 0.5;
+  double _speed = 1.0;
+  double _rockTime = 0.0;
+  bool _isPaused = false;
+
+  ActorAnimation _rock;
+
+  @override
+  void initialize(FlutterActorArtboard artboard) {
+    _rock = artboard.getAnimation("music_walk");
+  }
+
+
+  @override
+  bool advance(FlutterActorArtboard artboard, double elapsed) {
+    _rockTime += elapsed * _speed;
+    _rock.apply(_rockTime % _rock.duration, artboard, _rockAmount);
+    return true;
+  }
+
 
   @override
   void initState() {
     super.initState();
- //   getRemoteConfg();
-
     rippleBackground1 = RippleBackground(
       isPlaying: true,
       onComplete: () {
@@ -79,13 +100,9 @@ class _MyHomePageState extends State<SplashScreen>
     });
   }
 
-
-
   @override
   Widget build(BuildContext context) {
    // util.getDeviceMetrics(context);
-
-
     return splashPage();
   }
   Widget splashPage(){
@@ -93,52 +110,17 @@ class _MyHomePageState extends State<SplashScreen>
       body: Stack(
         //alignment: Alignment.center,
         children: <Widget>[
-
-     /*     Positioned.fill(
+          Positioned.fill(
               child: FlareActor("images/Penguin.flr",
                   alignment: Alignment.center,
-                  isPaused: false,
+                  isPaused: _isPaused,
                   fit: BoxFit.cover,
-                  animation: "walk")),*/
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.bottomLeft,
-                colors: [Color(0xff1E5146),Color(0xff047035)],
-                tileMode:
-                TileMode.clamp, // repeats the gradient over the canvas
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: /*Image.asset(
-              'images/pichai.jpg',
-              height: scaleBounceAnim.value,
-            ),*/
-            FlareActor("images/Penguin.flr",
-                alignment: Alignment.center,
-                isPaused: false,
-                fit: BoxFit.cover,
-                animation: "walk")
-          ),
+                  animation: "walk",controller: this,)),
 
-          Align(
-              alignment: Alignment(0.0, 0.35),
-              child: Text(
-                'Free Technical Courses | Developer News\n Live Mentorship',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0),
-              )),
           Positioned(
-            top: - MediaQuery.of(context).size.height / 2,
+            top: - MediaQuery.of(context).size.height / 1.75,
             left: - MediaQuery.of(context).size.width / 2,
-            height:  MediaQuery.of(context).size.height * 2,
+            height:  MediaQuery.of(context).size.height *2,
             width:  MediaQuery.of(context).size.width * 2,
             child: Stack(
               children: <Widget>[
@@ -150,47 +132,70 @@ class _MyHomePageState extends State<SplashScreen>
               ],
             ),
           ),
-     /*     Container(
-            height: 200,
-            width: 200,
-            child: FlareActor("images/intro(1).flr2d",alignment:Alignment.center, fit:BoxFit.contain, animation:"coding"),
-          ),*/
-          Align(
-            alignment: Alignment(0.0, 0.8),
-            child: GestureDetector(
-              onTap: () {
-                //   print('click');
-                //  Navigator.pop(context);
 
-              },
-              child: Container(
-                alignment: Alignment.center,
-                decoration: new BoxDecoration(
-                    color: Colors.white,
-                    borderRadius:
-                    new BorderRadius.all(Radius.circular(40.0))),
-                height:70.0,
-                width: 200.0,
-                child: Text(
-                  'Get Started !',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25.0),
-                ),
-              ),
-            ),
-          ),
+          Positioned.fill(
+              child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                        height: 200,
+                        color: Colors.black.withOpacity(0.5),
+                        child: new Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            new Text("Mix Amount",
+                                style: TextStyle(color: Colors.white)),
+                            new Slider(
+                              value: _rockAmount,
+                              min: 0.0,
+                              max: 1.0,
+                              divisions: null,
+                              onChanged: (double value) {
+                                setState(() {
+                                  _rockAmount = value;
+                                });
+                              },
+                            ),
+                            new Text("Speed",
+                                style: TextStyle(color: Colors.white)),
+                            new Slider(
+                              value: _speed,
+                              min: 0.2,
+                              max: 3.0,
+                              divisions: null,
+                              onChanged: (double value) {
+                                setState(() {
+                                  _speed = value;
+                                });
+                              },
+                            ),
+                            new Text("Paused",
+                                style: TextStyle(color: Colors.white)),
+                            new Checkbox(
+                              value: _isPaused,
+                              onChanged: (bool value) {
+                                setState(() {
+                                  _isPaused = value;
+                                });
+                              },
+                            )
+                          ],
+                        )),
+                  ])),
         ],
       ),
     );
   }
-
-
 
   @override
   void dispose() {
     controller.dispose();
     super.dispose();
   }
+  @override
+  void setViewTransform(Mat2D viewTransform) {
+    // TODO: implement setViewTransform
+  }
 }
+
+
